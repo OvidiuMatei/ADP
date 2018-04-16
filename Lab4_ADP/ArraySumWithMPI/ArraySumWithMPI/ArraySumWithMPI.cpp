637c201c6ed7380c2be8a6fb3fd3d0b881bf078f
OvidiuM <ovidiumatei736@gmail.com>
2018-04-16 21:16:20 +0300

    Bugged source

---
+++ Lab4_ADP/ArraySumWithMPI/ArraySumWithMPI/ArraySumWithMPI.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <random>
+
+#define N 20
+#define MASTER 0
+
+int main(int argc, char *argv[]) {
+
+	int rank, nrProcs, i, totalSum = 0, slice;
+	int array[N];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nrProcs);
+
+	slice = N / nrProcs;
+
+	for (i = 0; i < N; i++)
+	{
+		array[i] = rand() % 10 + 1;
+	}
+
+	if (rank != MASTER){
+		int tempSum = 0;
+
+		for (i = 0; i < slice; i++) {
+			tempSum += array[rank * slice + i];
+		}
+
+		printf("Rank #%d sending value %d\n", rank, tempSum);
+		MPI_Send(&tempSum, 1, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
+
+	}
+	else {
+		printf("MASTER receiving values:\n");
+
+		int auxSum;
+
+		for (i = 0; i < nrProcs; i++) {
+			MPI_Recv(&auxSum, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			printf("Received %d from rank #%d\n", auxSum, i);
+
+			totalSum += auxSum;
+		}
+
+		printf("Total Sum = %d\n", totalSum);
+	}
+	MPI_Finalize();
+}
